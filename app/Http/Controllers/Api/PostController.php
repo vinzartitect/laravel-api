4ce30182c30d56ce4a98ad83f0bd03e0d2918fcd
VincenzoBorriello89 <vincborr89@gmail.com>
2022-06-15 16:02:58 +0200

    mod controller

--- app/Http/Controllers/Api/PostController.php
+++ app/Http/Controllers/Api/PostController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use App\Models\Post;
 
 class PostController extends Controller
 {
@@ -14,7 +15,9 @@ class PostController extends Controller
      */
     public function index()
     {
-        //
+        $posts = Post::all();
+
+        return response()->json( compact('posts') );
     }
 
     /**
